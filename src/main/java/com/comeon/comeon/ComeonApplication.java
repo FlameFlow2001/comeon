79307c8e5cb37a15e29ffe8754774d9936ae47b8
MainUser <work4ksh@gmail.com>
2026-01-28 11:43:44 +0300

    Init commit

---
+++ src/main/java/com/comeon/comeon/ComeonApplication.java
@@ -0,0 +1,13 @@
+package com.comeon.comeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comeon/comeon/ComeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comeon.comeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
