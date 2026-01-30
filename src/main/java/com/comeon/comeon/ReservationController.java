6d8362a123ead7e508e839bd1a5fdec127fb0b96
Shundev Kirill <work4ksh@gmail.com>
2026-01-30 16:20:18 +0300

    Get by ID and get all reservations methods are done

--- src/main/java/com/comeon/comeon/ReservationController.java
+++ src/main/java/com/comeon/comeon/ReservationController.java
@@ -1,11 +1,18 @@
 package com.comeon.comeon;
 
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
 @RestController
-public class ReservationController {
+public class ReservationController
+{
+    private static final Logger log = getLogger(ReservationController.class);
     private final ReservationService reservationService;
 
     public ReservationController(ReservationService reservationService){
@@ -13,7 +20,12 @@ public class ReservationController {
     }
 
     @GetMapping("/{id}")
-    public Reservation GetReservationById(@PathVariable("id") Long id){
+    public Reservation getReservationById(@PathVariable("id") Long id){
         return reservationService.getReservationServiceById(id);
+    }
+    @GetMapping()
+    public List<Reservation> getAllReservations(){
+        log.info("Called getAllReservations method");
+        return reservationService.findAllReservations();
     }
 }
--- src/main/java/com/comeon/comeon/ReservationService.java
+++ src/main/java/com/comeon/comeon/ReservationService.java
@@ -1,13 +1,50 @@
 package com.comeon.comeon;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Service
 public class ReservationService {
 
+    private final Map<Long, Reservation> reservationMap = Map.of(
+            1L, new Reservation(1L,
+            100L,
+            40L,
+            LocalDate.now(),
+            LocalDate.now().plusDays(5),
+            ReservationStatus.APPROVED
+            ),
+            2L, new Reservation(2L,
+                    102L,
+                    42L,
+                    LocalDate.now(),
+                    LocalDate.now().plusDays(5),
+                    ReservationStatus.APPROVED
+            ),
+            3L, new Reservation(3L,
+                    103L,
+                    43L,
+                    LocalDate.now(),
+                    LocalDate.now().plusDays(5),
+                    ReservationStatus.APPROVED
+            )
+    );
+
+    @GetMapping("/{id}")
     public Reservation getReservationServiceById(Long id) {
-        return new Reservation(id, 100L, 40L, LocalDate.now(), LocalDate.now().plusDays(5), ReservationStatus.APPROVED);
+        if (!reservationMap.containsKey(id)){
+            throw new NoSuchElementException("Reservation with ID" + id + " was not found");
+        }
+        return reservationMap.get(id);
+    }
+
+    @GetMapping()
+    public List<Reservation> findAllReservations() {
+        return reservationMap.values().stream().toList();
     }
 }
